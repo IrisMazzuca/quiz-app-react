376468df79f2e6c0c2f0f12598eb9367c8bf5cfb
Iris Mazzuca <irismazzuca@gmail.com>
2020-08-09 16:52:06 -0300

    Updates

---
+++ precache-manifest.3d698ef1ed9d193afa7687c21b9bc1b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e7ee8942f9e185033fc5b7c2a85a79",
+    "url": "/quiz-app-react/index.html"
+  },
+  {
+    "revision": "64ddd593d4a1cf45301b",
+    "url": "/quiz-app-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3f07187320860f8049b7",
+    "url": "/quiz-app-react/static/css/main.39a5122e.chunk.css"
+  },
+  {
+    "revision": "64ddd593d4a1cf45301b",
+    "url": "/quiz-app-react/static/js/2.18be9004.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app-react/static/js/2.18be9004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f07187320860f8049b7",
+    "url": "/quiz-app-react/static/js/main.634e07a3.chunk.js"
+  },
+  {
+    "revision": "96536046ff39f927a38f",
+    "url": "/quiz-app-react/static/js/runtime-main.123f678d.js"
+  }
+]);
